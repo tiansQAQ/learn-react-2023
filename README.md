4696e07c6887d2d4be6ab1ed736c108ce8c507c8
tians <haiandao2@gmail.com>
2023-05-17 18:03:20 +0800

    initial

--- README.md
+++ README.md
@@ -1,2 +1,15 @@
 # learn-react-2023
+
 learn react from 0 to 1
+
+## 1. npm init
+
+## 2. npm i react react-dom
+
+## 3. npm i @babel/core @babel/cli @babel/preset-env @babel/preset-react --save-dev
+
+## 4. npm i webpack webpack-cli webpack-dev-server --save-dev
+
+## 5. npm i babel-loader --save-dev
+
+## 6. npm i html-webpack-plugin --save-dev
---
+++ webpack.config.js
@@ -0,0 +1,33 @@
+const path = require('path')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+
+module.exports = {
+  mode: 'development',
+  // 入口文件
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  },
+
+  // webpack 只能理解 JavaScript 和 JSON 文件，加载别的文件类型需要loader
+  module: {
+    rules: [
+      // 使用babel-loader转换jsx
+      {
+        test: /\.js$/,
+        use: 'babel-loader',
+        exclude: /node_modules/
+      }
+    ]
+  },
+  // html-webpack-plugin 为应用程序生成一个 HTML 文件，并自动将生成的所有 bundle 注入到此文件中。
+  plugins: [new HtmlWebpackPlugin({ template: './public/index.html' })],
+
+  resolve: {
+    // 别名
+    alias: {
+      '@': path.join(__dirname, 'src')
+    }
+  }
+}
